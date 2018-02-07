0c9a65500b5139f04eacf1aeee21263be22bb3f7
bsneha <bsneha@thoughtworks.com>
2018-02-07 15:19:09 +0530

    Changing to use uri instead of local html content

--- App.js
+++ App.js
@@ -5,27 +5,12 @@ import {
   StyleSheet,
   Platform
 } from 'react-native';
-
-import Widget from './external/widget/index.html'
 const isAndroid= Platform.OS==='android'
-const html =`<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" type="text/css" href="./widget/css/styles.css"/>
-</head>
 
-<body >
-    <h1>Web view with local html</h1>
-    <p id="para1">Some content</p>
-    <p>Some more content</p>
-</body>
-
-</html>`
 export default class App extends Component {
-
   render() {
     return (
-        <WebView source={{html:html, baseUrl:isAndroid?'file:///android_asset/':'./external'}}/>
+      <WebView source={{uri:isAndroid?'file:///android_asset/widget/index.html':'./external/widget/index.html'}}/>
     );
   }
 }
